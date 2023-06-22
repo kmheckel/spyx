75923dfcaba692a0e8036f53a3ea862faa0d5c0b
Kade Heckel <kade.heckel@gmail.com>
2023-06-22 08:34:30 +0100

    fixing PyPI stuff

--- setup.py
+++ setup.py
@@ -9,8 +9,7 @@ from os import path
 HERE = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+long_description = "Spyx is a compact library built on top of DeepMind's Haiku library, enabling easy construction of spiking neural network models."
 
 
 requires = (
@@ -25,11 +24,11 @@ requires = (
 # This call to setup() does all the work
 setup(
     name="spyx",
-    version="0.0.1",
+    version="0.0.1.1",
     description="Spyx: SNNs in JAX",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/kmheckel/spyx",
     author="Kade Heckel",
     author_email="example@email.com",
     license="MIT",
@@ -38,13 +37,12 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent"
     ],
-    packages=["medium_multiply"],
+    packages=["spyx"],
     include_package_data=True,
     install_requires=requires
 )
