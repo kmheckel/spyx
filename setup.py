8514378c29473ed2fc399a0a62e4cf2ad5e1dec0
Kade Heckel <kade.heckel@gmail.com>
2023-08-11 15:23:55 +0100

    v0.0.44

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requires = (
 # This call to setup() does all the work
 setup(
     name="spyx",
-    version="0.0.43",
+    version="0.0.44",
     description="Spyx: SNNs in JAX",
     long_description=long_description,
     long_description_content_type="text/markdown",
