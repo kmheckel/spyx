faa92d661a929586fb23df2d96b75b3b78c53f68
Gregor Lenz <mail@lenzgregor.com>
2023-10-21 14:00:01 +0100

    docs: use sphinx autoapi, myst-nb, viewcode, move examples folder to docs

--- docs/conf.py
+++ docs/conf.py
@@ -20,18 +20,50 @@ release = 'v0.1.8'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'recommonmark',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode'
+    "autoapi.extension",
+    "myst_nb",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
+
+autodoc_typehints = "both"
+autoapi_type = "python"
+autoapi_dirs = ["../spyx"]
+autoapi_options = [
+    "members",
+    "undoc-members",
+    "show-inheritance",
+    "show-module-summary",
+    "special-members",
+    "imported-members",
+]
+
+nb_execution_mode = "off"
+nb_execution_timeout = 300
+nb_execution_show_tb = True
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_book_theme"
+html_title = "Spyx"
+html_logo = "../spyx.png"
+html_show_sourcelink = True
+html_sourcelink_suffix = ""
+
+html_theme_options = {
+    "repository_url": "https://github.com/neuromorphs/tonic",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "repository_branch": "develop",
+    "path_to_docs": "docs",
+    "use_fullscreen_button": True,
+}
+
+# html_static_path = ['_static']
--- docs/source/api/nn.rst
+++
@@ -1,7 +0,0 @@
-spyx.nn
-=======
-
-.. automodule:: spyx.nn
-   :members:
-   :special-members:
-   :undoc-members:
