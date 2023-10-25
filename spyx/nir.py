577edf4da90d5eb833c770eaa8927fdd93c656d0
Kade Heckel <51248168+kmheckel@users.noreply.github.com>
2023-10-25 22:29:59 +0100

    Update nir.py

--- spyx/nir.py
+++ spyx/nir.py
@@ -1,5 +1,7 @@
 import nir
-
+import numpy as np
+import haiku as hk
+import .nn
 
 def _nir_node_to_spyx_node(node: nir.NIRNode):
     """Converts a NIR node to a Spyx node."""
@@ -12,10 +14,12 @@ def _nir_node_to_spyx_node(node: nir.NIRNode):
     elif isinstance(node, nir.Affine):
         # NOTE: node.weight, node.bias are npy arrays
         pass
+        # return hk.Linear()
 
     elif isinstance(node, nir.Linear):
         # NOTE: node.weight
         pass
+        # return hk.Linear()
 
     elif isinstance(node, nir.Conv1d):  # not needed atm
         # NOTE: node.bias, node.weight
@@ -66,7 +70,7 @@ def to_nir(spyx_pytree, input_shape, output_shape) -> nir.NIRGraph:
     # construct the edge list for the NIRGraph
     keys = list(spyx_pytree.keys())
     edges = [(keys[i], keys[i + 1]) for i in range(len(keys) - 1)] # assume linear connectivity
-    edges.insert(("input", edges[0][0]), 0)
+    edges.insert(0, ("input", edges[0][0]))
     edges.append((edges[-1][1], "output"))
 
     # begin constructing the node list:
@@ -75,29 +79,27 @@ def to_nir(spyx_pytree, input_shape, output_shape) -> nir.NIRGraph:
         "output" : nir.Output(output_shape)
     }
 
-    try:
-        for layer, params in spyx_pytree.items():
-            layer_type = layer.split("_")[0]
-            if layer_type == "linear":
-                if "b" in params:
-                    nodes[layer] = nir.Affine(params["w"], params["b"])
-                else:
-                    nodes[layer] = nir.Linear(params["w"])
-            elif layer_type == "conv2d":
-                pass # nodes[layer] = nir.Conv2d()
-            elif layer_type == "IF":
-                nodes[layer] = nir.IF(r=1, v_threshold=1)
-            elif layer_type == "LIF":
-                nodes[layer] = nir.LIF(
-                    tau=1/(1-params["b"]),
-                    v_threshold=1,
-                    v_leak=0,
-                    r=params["b"]
-                )
+    for layer, params in spyx_pytree.items():
+        layer_type = layer.split("_")[0]
+        if layer_type == "linear":
+            if "b" in params:
+                nodes[layer] = nir.Affine(np.array(params["w"]), np.array(params["b"]))
             else:
-                raise
-    except:
-        print("Attempted exportation of a model which contains a layer not support by NIR.")
+                nodes[layer] = nir.Linear(np.array(params["w"]))
+        elif layer_type == "conv2d":
+            pass # nodes[layer] = nir.Conv2d()
+        elif layer_type == "IF":
+            nodes[layer] = nir.IF(r=1, v_threshold=1)
+        elif layer_type == "LIF":
+            nodes[layer] = nir.LIF(
+                tau=1/(1-np.array(params["beta"])),
+                v_threshold=np.ones_like(params["beta"]),
+                v_leak=np.zeros_like(params["beta"]),
+                r=np.array(params["beta"])
+            )
+        else:
+            print("Attempted exportation of a model which contains a layer not support by NIR.")
+            print("Unsupported layer name was not added to NIRGraph:", layer)
 
     return nir.NIRGraph(nodes, edges)
     
@@ -108,4 +110,8 @@ def from_nir(nir_graph: nir.NIRGraph):
     # NOTE: iterate over nir_graph, convert each node to a Spyx module
     # (using _nir_node_to_spyx_node)
     # could do this cleanly by using a list comprehension on the NIRGraph and then passing the list to the hk.RNNCore constructor.
+    # actually, this might be more complicated. Might want to create entire haiku function in here and transform it, returning
+    # just the pure function object and the associated parameter pytree. Could make things a lot cleaner.
     pass
+    # for every non-input/output node, convert to a spyx node and return in list form.
+    # return [ _nir_node_to_spyx_node(nir_graph.nodes[n[0]]) for n in nir_graph.edges[1:] ]
