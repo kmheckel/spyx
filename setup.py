84b6a6a0a6c75afeaa1c7b47a1c2f5026f3be1af
Kade Heckel <kade.heckel@gmail.com>
2024-08-16 10:23:13 +0100

    version bump

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ extras = {
 # This call to setup() does all the work
 setup(
     name="spyx",
-    version="0.1.19",
+    version="0.1.20",
     description="Spyx: SNNs in JAX",
     long_description=long_description,
     long_description_content_type="text/markdown",
